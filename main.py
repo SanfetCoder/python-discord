916cd1e98a78234de1426f8d61dd5bb0f50cafe6
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-01-26 10:13:19 +0700

    Able to read file from users

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 client = discord.Client(intents=intents)
@@ -22,6 +27,13 @@ async def on_message(message):
     # Check if the message has image attachments
     for attachment in message.attachments:
       if attachment.content_type.startswith('image'):
+        # Download the file
+        saved_file = await attachment.save('downloaded_image.png')
+        
+        # Send the file back as a message
+        with open('downloaded_image.png', 'rb') as f:
+          await message.channel.send(file=discord.File(f))
+          
         await message.channel.send(f'Thanks for the image, {message.author.name}!')
       else:
         await message.channel.send('Please upload a valid image.')
@@ -31,4 +43,4 @@ async def smartest_guy(ctx):
   await ctx.send('Yang!')
 
 
-client.run()
+client.run(os.environ["DISCORD_TOKEN"])
